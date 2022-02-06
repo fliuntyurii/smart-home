be7af6e7a74b4407d2a85983befa4fe010efb4e3
fliuntyurii <88083323+fliuntyurii@users.noreply.github.com>
2022-02-06 19:29:25 +0200

    Comment

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,4 @@
 (function() {
-    const bodyScrollLock = document.querySelector('.modal-open');
     const popupButtonYes = document.querySelector('.popup-confirm');
     const popupWrapper = document.querySelector('.popup')
     function popupButtonYesClick(){
